9564c047c03b871388ed394e531bc7f0105091fa
anas <anas.ejaz1991@gmail.com>
2017-05-27 15:16:58 +0500

    send text and image using whats up application

--- SendWhatsPic/ViewController.swift
+++ SendWhatsPic/ViewController.swift
@@ -9,17 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var docs = UIDocumentInteractionController()
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func btnPressed(_ sender: UIButton) {
+        
+        self.sendPicWhatsup()
+
+        //self.senbyActivity()
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
+        
     }
 
+    func senbyActivity(){
+        let shareText = "Hello, world!"
+        
+        if let image = UIImage(named: "image.png") {
+        
+            let vc = UIActivityViewController(activityItems: [shareText, image], applicationActivities: [])
+            self.present(vc, animated: true, completion: nil)
+        }
+    
+    }
+    func sendPicWhatsup(){
 
+    let urlWhats = "whatsapp://app"
+        
+        if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed){
+        
+            if let whatsupURL = URL(string: urlString){
+            
+                if UIApplication.shared.canOpenURL(whatsupURL){
+                
+                    if let image = UIImage(named: "image.png"){
+                    
+                       if let imageData = UIImagePNGRepresentation(image)
+                        
+                       {
+                          let tempfile = URL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent("Documents/whatAppTmp.wai")
+                        do{
+                        
+                            try imageData.write(to: tempfile, options: .atomicWrite)
+                            self.docs = UIDocumentInteractionController(url: tempfile)
+                            self.docs.uti = "net.whatsapp.iamge"
+                            self.docs.presentOpenInMenu(from: CGRect.zero, in: self.view, animated: true)
+                            
+                            
+                        
+                        }catch let error {
+                        
+                        print(error)
+                        }
+                        
+                        
+                        }
+                    
+                    }
+                    
+                
+                
+                }
+            
+            
+            }
+            
+            
+        
+        }
+        
+        
+    
+    }
 }
 
